082b29d1f0d412ac077ce4a80582f1c463343981
Markus Binsteiner <markus.binsteiner@arcs.org.au>
2009-03-16 22:12:18 +0000

    Fixed script.
    git-svn-id: http://projects.arcs.org.au/svn/grisu/trunk/grisu@1022 8882a08a-ce34-0410-b810-d8c7c368afb5

--- fix-build.sh
+++ fix-build.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 
+cp ../backend/grisu-ws/target/grisu-ws.war .
+
 cd target
 mkdir swing
 cd swing
@@ -10,7 +12,7 @@ jar cmf ../../frontend/grisu-client-swing/MANIFEST.MF ../grisu.jar .
 cd ..
 mkdir cmdline
 cd cmdline
-unzip -o ./grisu-0.2-beta-SNAPSHOT-grisu-client-commandline.jar
+unzip -o ../grisu-0.2-beta-SNAPSHOT-grisu-client-commandline.jar
 rm META-INF/INDEX.LIST
 jar cmf ../../frontend/grisu-client-commandline/MANIFEST.MF ../grisu-client.jar .
 

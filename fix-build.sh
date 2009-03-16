1fb215120860f99222cde1a8ea5ac6b06fdd502f
Markus Binsteiner <markus.binsteiner@arcs.org.au>
2009-03-16 23:14:35 +0000

    Small fixes.
    git-svn-id: http://projects.arcs.org.au/svn/grisu/trunk/grisu@1027 8882a08a-ce34-0410-b810-d8c7c368afb5

--- fix-build.sh
+++ fix-build.sh
@@ -1,11 +1,14 @@
 #! /bin/bash
 
+cd target
+
+mv grisu-0.2-beta-SNAPSHOT-grisu-local-backend.jar grisu-local-backend.jar
 cp ../backend/grisu-ws/target/grisu-ws.war .
 
-cd target
 mkdir swing
 cd swing
 unzip -o ../grisu-0.2-beta-SNAPSHOT-grisu-client-swing.jar
+rm ../grisu-0.2-beta-SNAPSHOT-grisu-client-swing.jar
 rm META-INF/INDEX.LIST
 jar cmf ../../frontend/grisu-client-swing/MANIFEST.MF ../grisu.jar .
 
@@ -13,6 +16,7 @@ cd ..
 mkdir cmdline
 cd cmdline
 unzip -o ../grisu-0.2-beta-SNAPSHOT-grisu-client-commandline.jar
+rm ../grisu-0.2-beta-SNAPSHOT-grisu-client-commandline.jar
 rm META-INF/INDEX.LIST
 jar cmf ../../frontend/grisu-client-commandline/MANIFEST.MF ../grisu-client.jar .
 
